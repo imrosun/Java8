4ca40f309ea982c3e3f8f2308296a2a97b2981d7
Roshan Sharma <imrosun@gmail.com>
2022-07-03 08:02:12 +0530

    Added Wiggle Subsequence

---
+++ src/Practice/WiggleSubsequence.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class WiggleSubsequence {
+    public int wiggleMaxLength(int[] nums) {
+        if(nums.length<2)return 1;
+
+        int count=1;
+        int prevDiff=0;
+        for(int i=1;i<nums.length;i++){
+            int diff = nums[i] - nums[i-1];
+            // the equals in prevDiff <= 0 && prevDiff >=0 can only be used at the first iteration, otherwise prevDiff will never be zero afterwards
+            if((diff>0 && prevDiff <= 0) || (diff<0 && prevDiff >= 0)){
+                count++;
+                prevDiff = diff;
+            }
+        }
+        return count;
+    }
+}
